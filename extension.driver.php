31c76c02383fc4a5ad7a55b9106280edc0474eb1
Nils Hörrmann <post@nilshoerrmann.de>
2013-02-19 11:21:45 +0100

    Create Data Source interface

---
+++ extension.driver.php
@@ -0,0 +1,29 @@
+<?php
+
+	require_once EXTENSIONS . '/storage/data-sources/datasource.storage.php';
+
+	Class Extension_Storage extends Extension {
+
+		private static $provides = array();
+
+		public static function registerProviders() {
+			self::$provides = array(
+				'data-sources' => array(
+					'StorageDatasource' => StorageDatasource::getName()
+				)
+			);
+
+			return true;
+		}
+
+		public static function providerOf($type = null) {
+			self::registerProviders();
+
+			if(is_null($type)) return self::$provides;
+
+			if(!isset(self::$provides[$type])) return array();
+
+			return self::$provides[$type];
+		}
+
+	}
