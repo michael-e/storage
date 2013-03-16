michael-e/storage

commit 1c61cdab3e7771bedad4499f00f908c12173f222

It should be safe to the use 'count' key only.
No matter where we implement this, we must ensure that the session array will not include 'count-positive' keys. So it is safe to use 'count' exclusively.

lib/class.storage.php
@@ -235,7 +235,7 @@
                 }
 
                 // Count as attribute
-                elseif( ($key == 'count' || $key == 'count-positive') && $count_as_attribute === true ) {
+                elseif( ($key == 'count' && $count_as_attribute === true ) {
                     if(empty($value)) $value = 0;
                     $parent->setAttribute('count', $value);
                 }

commit 837c7996096e3a5aae672a36483a9ed5def657bf

Fix typo introduced in 1c61cdab3e7771bedad4499f00f908c12173f222

lib/class.storage.php
@@ -240,7 +240,7 @@
                 }
 
                 // Count as attribute
-                elseif( ($key == 'count' && $count_as_attribute === true ) {
+                elseif($key == 'count' && $count_as_attribute === true) {
                     if(empty($value)) $value = 0;
                     $parent->setAttribute('count', $value);
                 }