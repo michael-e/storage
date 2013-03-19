0cfeb8419497a3c2cc546bfc870495e4e5fb5bb0
michael-e <info@michael-eichelsdoerfer.de>
2013-03-19 14:24:49 +0100

    Add about function to the extension driver.

--- extension.driver.php
+++ extension.driver.php
@@ -5,6 +5,24 @@
     Class Extension_Storage extends Extension {
 
         /**
+         * About function
+         *
+         * @return array
+         */
+        public function about() {
+            return array(
+                'name' => 'Storage',
+                'version' => '1.0',
+                'release-date' => '2013-03-19',
+                'author'       => array(
+                    'name'    => 'Michael Eichelsdoerfer',
+                    'website' => 'http://www.michael-eichelsdoerfer.de',
+                    'email'   => 'info@michael-eichelsdoerfer.de'
+                )
+            );
+        }
+
+        /**
          * Delegates and callbacks
          *
          * @return array
