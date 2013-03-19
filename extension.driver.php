8f74113734e30a5937bb6b18c6c81eb71ee69983
michael-e <info@michael-eichelsdoerfer.de>
2013-03-19 12:54:53 +0100

    Remove all 2.3 specific datasource code.
    Unfortunately, there is no elegant way to commit the datasource changes because the datasource file name must be different in the end (which will make Git see the changes as file removal/addition). So we drop everything in this commit and add a 2.2.x compatible datasource shortly after.

--- extension.driver.php
+++ extension.driver.php
@@ -1,31 +1,8 @@
 <?php
 
-    require_once EXTENSIONS . '/storage/data-sources/datasource.storage.php';
     require_once EXTENSIONS . '/storage/lib/class.storage.php';
 
-    Class extension_Storage extends Extension {
-
-        private static $provides = array();
-
-        public static function registerProviders() {
-            self::$provides = array(
-                'data-sources' => array(
-                    'StorageDatasource' => StorageDatasource::getName()
-                )
-            );
-
-            return true;
-        }
-
-        public static function providerOf($type = null) {
-            self::registerProviders();
-
-            if(is_null($type)) return self::$provides;
-
-            if(!isset(self::$provides[$type])) return array();
-
-            return self::$provides[$type];
-        }
+    Class Extension_Storage extends Extension {
 
         /**
          * Delegates and callbacks
