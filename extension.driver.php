890c6a0f1604084fc80cc346b038afc7e6f2465b
Nils Hörrmann <post@nilshoerrmann.de>
2013-04-10 18:52:43 +0200

    Add event filter that saves the event's field data in the storage
    See #9

--- extension.driver.php
+++ extension.driver.php
@@ -26,4 +26,43 @@
             return self::$provides[$type];
         }
 
+        public function getSubscribedDelegates() {
+            return array(
+                array(
+                    'page' => '/blueprints/events/edit/',
+                    'delegate' => 'AppendEventFilter',
+                    'callback' => 'appendEventFilter'
+                ),
+                array(
+                    'page' => '/blueprints/events/new/',
+                    'delegate' => 'AppendEventFilter',
+                    'callback' => 'appendEventFilter'
+                ),
+                array(
+                    'page' => '/frontend/',
+                    'delegate' => 'EventFinalSaveFilter',
+                    'callback' => 'eventFinalSaveFilter'
+                )
+            );
+        }
+        
+        public function appendEventFilter($context) {
+            $handle = 'storage-add';
+            $selected = (in_array($handle, $context['selected']));
+            $context['options'][] = Array(
+                $handle, $selected, __('Add to Storage')
+            );
+        }
+
+        public function eventFinalSaveFilter($context) {
+            $storage = new Storage();
+            $storage->set(
+                array(
+                    'events' => array(
+                        $context['event']->ROOTELEMENT => $context['fields']
+                    )
+                )
+            );
+        }        
+
     }
